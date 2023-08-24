bc232ef8777b971f02caaf8df4af3bb0a402ac7e
xuanwei <xuanwei@exocr.com>
2023-08-24 19:02:46 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)
+        ),
+        home: MyHomepage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+        body: Column(
+      children: [
+        Text('A random idea:'),
+        Text(appState.current.asLowerCase),
+      ],
+    ));
+  }
+}
